f7a8f8f3f63161bc4e5b326709f2fbbea1e3df6f
ShivaniMerchant <145406880+merchShivani@users.noreply.github.com>
2023-10-01 02:54:44 -0500

    AddPlayersToGame.java

---
+++ AddPlayersToGame.java
@@ -0,0 +1,31 @@
+public class AddPlayersToGame {
+    public static void main(String[] args) {
+        // Create a game instance
+        Game game = new Game();
+
+        // Start the game loop
+        game.run();
+
+        // Simulate adding two players
+        addPlayersToGame(game.getModel());
+
+        // Close the game after adding players (you can adjust this based on your game logic)
+        game.closeGame();
+    }
+
+    // Simulate adding two players to the game
+    private static void addPlayersToGame(Model model) {
+        // Open the player entry window
+        model.openPlayerWindow();
+
+        // Simulate adding Player 1
+        model.setPlayerID("1"); // Set the player ID as needed
+        model.setPlayerCodeName("Player1"); // Set the player code name as needed
+        model.createPlayer();
+
+        // Simulate adding Player 2
+        model.setPlayerID("2"); // Set the player ID as needed
+        model.setPlayerCodeName("Player2"); // Set the player code name as needed
+        model.createPlayer();
+    }
+}
