76d1640896032a6b38f2225f68f6e4d80f9c1432
ShivaniMerchant <145406880+merchShivani@users.noreply.github.com>
2023-10-24 00:03:12 -0500

    Update View.java

--- View.java
+++ View.java
@@ -14,7 +14,6 @@ class View extends JPanel {
 	BufferedImage photonSplash;
 	BufferedImage photonPlayerLogin;
 	BufferedImage photonGameplay;
-	BufferedImage playerActionBg;
 
 	View(Controller c, Model m) {
 		model = m;
@@ -22,7 +21,6 @@ class View extends JPanel {
 			this.photonSplash = ImageIO.read(new File("Photon_Resize.jpg"));
 			this.photonPlayerLogin = ImageIO.read(new File("Photon_Player_Select15.png"));
 			this.photonGameplay = ImageIO.read(new File("Photon_Gameplay_Placeholder.jpg"));
-			this.playerActionBg = ImageIO.read(new File("player_action_bg.png"));
 		} catch (Exception e) {
 			e.printStackTrace(System.err);
 			System.exit(1);
@@ -85,16 +83,75 @@ class View extends JPanel {
 				g.drawString(playerInfo.playerCodeName, playerInfo.menux + 300, playerInfo.menuy + 10);
 			}
 		}
-		// Creates countdown once this game phase is hit
+
 		if (model.gamePhase == 2) {
-			g.drawImage(this.photonGameplay, 0, 0, this.getWidth(), this.getHeight(), null);
+			String counterText = Integer.toString(model.gameStartCountdown);
+			g.setColor(new Color(250, 0, 0));
+			g.fillRect(0, 0, this.getWidth(), this.getHeight());
+			g.setColor(new Color(250, 250, 250));
+			g.setFont(new Font("Times New Roman", Font.BOLD, 200));
+			g.drawString(counterText, 550, 350);
 		}
 
-		// Draw this background image when model.gamePhase is 3
 		if (model.gamePhase == 3) {
-			g.drawImage(playerActionBg, 0, 0, getWidth(), getHeight(), null);
+			int teamBoxHeight = model.largestTeam * 30 + 60;
+			g.setFont(new Font("Times New Roman", Font.BOLD, 20));
+			g.setColor(new Color(0, 0, 0));
+			g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+			// Draw Events Window
+			g.setColor(new Color(0, 0, 250));
+			g.fillRect(100, 50, 1100, 600);
+
+			// Draw Teams Square
+			g.setColor(new Color(250, 250, 250));
+			g.fillRect(100, 50, 1100, teamBoxHeight);
+
+			// Draw Red Team Score
+			g.setColor(new Color(250, 0, 0));
+			String redTeamScore = Integer.toString(model.redTeamScore);
+			g.drawString("Red Team Score: ", 200, teamBoxHeight + 30);
+			g.drawString(redTeamScore, 400, teamBoxHeight + 30);
+
+			// Draw Green Team Score
+			g.setColor(new Color(0, 250, 0));
+			String greenTeamScore = Integer.toString(model.greenTeamScore);
+			g.drawString("Green Team Score: ", 750, teamBoxHeight + 30);
+			g.drawString(greenTeamScore, 950, teamBoxHeight + 30);
+
+			// Outline for game Area
+			g.setColor(new Color(200, 200, 200));
+			g.fillRect(100, 50, 20, 600);
+			g.fillRect(1200, 50, 20, 600);
+			g.fillRect(100, 50, 1100, 20);
+			g.fillRect(100, 650, 1120, 20);
+
+			// Line Between Team Window and Events Window
+			g.fillRect(100, teamBoxHeight + 40, 1100, 20);
+
+			for (int i = 0; i < model.playerList.size(); i++) {
+				playerInfo = model.playerList.get(i);
+
+				if (playerInfo.team == 0) {
+					g.setColor(new Color(250, 0, 0));
+				} else {
+					g.setColor(new Color(0, 250, 0));
+				}
+				// Holder for Player List
+
+				// Converts Int value of ID to a String
+				String playerScore = Integer.toString(playerInfo.currentScore);
+
+				// Draws PlayerScore
+				g.drawString(playerScore, playerInfo.menux + 200, playerInfo.menuy - 50);
+
+				// Draws PlayerCodeName
+				g.drawString(playerInfo.playerCodeName, playerInfo.menux + 100, playerInfo.menuy - 50);
+			}
+
+			g.setColor(new Color(255, 255, 255));
+
 
 		}
-
 	}
 }
